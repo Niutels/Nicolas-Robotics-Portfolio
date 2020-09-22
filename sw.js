c59c0386c819879e447fe6b26196f86afdf4e488
Niutels <brissonneau.nicolas@gmail.com>
2020-09-22 12:11:21 -0500

    Updates

--- sw.js
+++ sw.js
@@ -51,10 +51,6 @@ self.__precacheManifest = [
   },
   {
     "url": "polyfill-befed6d0260fcf0c5e1b.js"
-  },
-  {
-    "url": "manifest.json",
-    "revision": "64a22cff60fae5950da826899450e394"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
